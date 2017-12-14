2a0c0a94bad6afddf89e929e2683eb79e84af88b
John Hamm <johndhamm1@gmail.com>
2017-12-14 11:06:54 -0600

    router and paths added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './containers/home';
 import Planner from './containers/planner';
+import GoalTracker from './containers/goalTracker';
+
 
 class App extends Component {
   render() {
     return (
-      <Planner />
+			<BrowserRouter>
+				<div>
+					<Switch>
+						<Route path="/planner" component={Planner} />
+						<Route path="/goalTracker" component={GoalTracker} />
+						<Route path="/" component={Home} />
+					</Switch>
+				</div>
+			</BrowserRouter>
     );
   }
 }
